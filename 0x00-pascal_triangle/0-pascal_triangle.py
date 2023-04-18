2c0756c09866310ca7869312137b7abd637e7e1d
FelixOtienoArogo <felixarogo@gmail.com>
2023-04-18 18:37:27 +0300

    Pascal triangle implementation

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+0-pascal_traingle.
+
+Creating a pascal triangle for a given number n.
+"""
+import math
+
+
+def pascal_triangle(n):
+    """Return a list of integers representing the Pascal's triangle of n."""
+    triangle = [[]]
+    for i in range(n):
+        if i < n - 1:
+            triangle.append([])
+        for j in range(i + 1):
+            triangle[i].append(int(
+                math.factorial(i)/(math.factorial(i - j) * math.factorial(j))))
+    return triangle
