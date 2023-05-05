f1866d78bc740ec5e475fa8f9f270ace41a190db
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-05 19:59:56 +0300

    try

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 
-"""Minimum Operations."""
+'''
+Given a number n, write a method that calculates
+the fewest number of operations needed to result in
+exactly n H characters in the file.
+'''
 
 
-def minOperations(n: int) -> int:
-    """
-    Description.
-
-    ------------
-    Given a number n, write a method that calculates the fewest number
-    of operations needed to result in exactly n H characters in the file.
-    """
-    i: int = 1
-    op: int = 0
-    temp: int = i
-    while i < n:
-        if i == 1:
-            op += 2
-            i += 1
-        elif (n % i) == 0:
-            temp = i
-            i = i * 2
-            op += 2
-        else:
-            i = i + temp
-            op += 1
-    if i == n:
-        return op
-    return 0
+def minOperations(n):
+    '''
+    returns min operations to get n Hs
+    '''
+    result = 0
+    index = 2
+    if n < 2:
+        return 0
+    while (index < n + 1):
+        # Check if problem is evenly brekadownable
+        while n % index == 0:
+            # If so add number of smaller problems to the result
+            result += index
+            # Create the smaller problem needed to get to n
+            n /= index
+        index += 1
+    return result
