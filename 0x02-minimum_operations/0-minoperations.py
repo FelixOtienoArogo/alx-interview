6b63700a11c49bf98b5b86af2268423fcb90f836
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-05 20:08:42 +0300

    new

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,24 +7,17 @@ exactly n H characters in the file.
 '''
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     '''
     returns min operations to get n Hs
     '''
-    i: int = 1
-    op: int = 0
-    temp: int = i
-    while i < n:
-        if i == 1:
-            op += 2
-            i += 1
-        elif (n % i) == 0:
-            temp = i
-            i = i * 2
-            op += 2
-        else:
-            i = i + temp
-            op += 1
-    if i == n:
-        return op
-    return 0
+    op = 0
+    idx = 2
+    if n < 2:
+        return 0
+    while (idx < n + 1):
+        while n % idx == 0:
+            op += idx
+            n /= idx
+        idx += 1
+    return op
