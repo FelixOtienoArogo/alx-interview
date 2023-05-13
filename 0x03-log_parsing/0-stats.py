5ee5c158140b469bbd2159f231efdbcf1b036820
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-13 04:17:20 +0300

    ta

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,59 +1,59 @@
 #!/usr/bin/python3
 """
-log parsing
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
 """
-
 import sys
-import re
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def printstat(code: dic, size: int) -> None:
+def prn_stats():
     """
-    helper function to display stats
+    Function that print stats about log
     """
-    print(f"File size: {size}")
-    for i in sorted(code.keys()):
-        if code[i] != 0:
-            print(f"{i}: {code[i]}")
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
-line_reg = re.compile(
-    r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
-    r'\[(.+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$')
-count = 0
-size = 0
-codes = {"200": 0,
-         "301": 0,
-         "400": 0,
-         "401": 0,
-         "403": 0,
-         "404": 0,
-         "405": 0,
-         "500": 0}
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        count += 1
-        try:
-            size += int(line[-1])
-        except Exception:
-            pass
-
-        if line_reg.match(line):
-            lis = line.split()
-            count += 1
+if __name__ == "__main__":
+    cnt = 0
+    try:
+        """ Iter the standar input """
+        for data in sys.stdin:
             try:
-                size += int(line[-1])
-            except Exception:
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
+            except:
                 pass
-            try:
-                if lis[-2] in codes:
-                    codes[lis[-2]] += 1
-            except Exception:
-                pass
-            if count % 10 == 0:
-                printstat(codes, size)
-
-except KeyboardInterrupt:
-    printstat(codes, size)
-    raise
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
+    except KeyboardInterrupt:
+        prn_stats()
+        raise
+    else:
+        prn_stats()
