eceaeadfdc13f08cfff1880f0c219a4b2ac876ce
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-13 04:27:22 +0300

    sec

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,22 +15,30 @@ Example:
     500: 3
 """
 import sys
-stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
-        "403": 0, "404": 0, "405": 0, "500": 0}
+codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
 summ = 0
 
 
-def prn_stats():
+def printstats():
     """
     Function that print stats about log
     """
     global summ
 
     print('File size: {}'.format(summ))
-    stcdor = sorted(stcd.keys())
+    stcdor = sorted(codes.keys())
     for each in stcdor:
-        if stcd[each] > 0:
-            print('{}: {}'.format(each, stcd[each]))
+        if codes[each] > 0:
+            print('{}: {}'.format(each, codes[each]))
 
 
 if __name__ == "__main__":
@@ -41,8 +49,8 @@ if __name__ == "__main__":
             try:
                 fact = data.split(' ')
                 """ If there is a status code """
-                if fact[-2] in stcd:
-                    stcd[fact[-2]] += 1
+                if fact[-2] in codes:
+                    codes[fact[-2]] += 1
                 """ If there is a lenght """
                 summ += int(fact[-1])
             except:
@@ -50,10 +58,10 @@ if __name__ == "__main__":
             """ Printing control """
             cnt += 1
             if cnt == 10:
-                prn_stats()
+                printstats()
                 cnt = 0
     except KeyboardInterrupt:
-        prn_stats()
+        printstats()
         raise
     else:
-        prn_stats()
+        printstats()
