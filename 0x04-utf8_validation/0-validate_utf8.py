3d0dae6bec22f001acc7e444e6fda58696af855f
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-20 17:16:18 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""validUTF8"""
+"""validUTF8."""
 
 
 def validUTF8(data):
     """Determine if a given data set represents a valid UTF-8 encoding."""
-    if len(data) > 4 or len(data) < 1:
-        return False
+    mask = 1 << 7
+    for char in data:
+        if (mask & char) != 0:
+            return False
     return True
