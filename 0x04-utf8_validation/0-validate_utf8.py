2ee2aaf331c00d71ab4b2f44cc81ba3a8596b57b
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-20 19:06:17 +0300

    Final

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -20,8 +20,11 @@ def validUTF8(data):
             else:
                 msb = char >> 4
                 if msb ^ 14 == 0:
-                    msb2 = data[i + 1] >> 6
-                    msb3 = data[i + 2] >> 6
+                    try:
+                        msb2 = data[i + 1] >> 6
+                        msb3 = data[i + 2] >> 6
+                    except Exception:
+                        return False
                     if msb2 ^ 2 == 0 and msb3 ^ 2 == 0:
                         valid = True
                         i += 3
@@ -30,9 +33,12 @@ def validUTF8(data):
                 else:
                     msb = char >> 3
                     if msb ^ 30 == 0:
-                        msb2 = data[i + 1] >> 6
-                        msb3 = data[i + 2] >> 6
-                        msb4 = data[i + 3] >> 6
+                        try:
+                            msb2 = data[i + 1] >> 6
+                            msb3 = data[i + 2] >> 6
+                            msb4 = data[i + 3] >> 6
+                        except Exception:
+                            return False
                         if msb2 ^ 2 == 0 and msb3 ^ 2 == 0 and msb4 ^ 2 == 0:
                             valid = True
                             i += 4
